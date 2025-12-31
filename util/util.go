92acfb51451f5d7dcbd4f41dbfdf9de240dd76ef
Daeron <daeron@Daerons-MacBook-Air.local>
2025-12-30 23:38:33 -0800

    remove main

--- util/util.go
+++ util/util.go
@@ -2,15 +2,7 @@ package util
 
 import (
 	"fmt"
-
-	"github.com/gorilla/mux"
 )
-
-func main() {
-	r := mux.NewRouter()
-	fmt.Println("Hello, World!")
-	fmt.Println("Router initialized:", r)
-}
 
 func Test() {
 	fmt.Println("abc")
