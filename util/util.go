f525b853ac039effd3c030856f52c48115baf296
Daeron <daeron@Daerons-MacBook-Air.local>
2025-12-30 23:30:27 -0800

    test

--- util/util.go
+++ util/util.go
@@ -1,7 +1,17 @@
-package util
+package main
 
-import "fmt"
+import (
+	"fmt"
 
-func Util() {
-	fmt.Println("Test abc")
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	fmt.Println("Hello, World!")
+	fmt.Println("Router initialized:", r)
+}
+
+func Test() {
+	fmt.Println("abc")
 }
